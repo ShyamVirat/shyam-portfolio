a4a6846e6b335e9d538b1d7d43f3dddc8814464d
ShyamVirat <k_komaravolu@fanshaweonline.ca>
2023-09-27 16:43:22 -0400

    My portfolio

---
+++ src/data/WorkData.js
@@ -0,0 +1,37 @@
+// work data
+
+export const Work = [
+    {
+        id: 1,
+        name: "Colossus Consulting Inc",
+        role: "Senior Full Stack Developer",
+        description: "Bulit and managed multiple Full stack applications for a Bank and handled a different teams for multiple projects.",
+        tags: ["React", "Angular", "NodeJS", "MongoDB"],
+        duration: "March 2022 - Present",
+        demo: "https://agency-website-eta.vercel.app/",
+        github: "https://github.com/codebucks27/Agency-website"
+    },
+    {
+        id: 2,
+        name: "Tata Consultancy Services",
+        role: "Senior Front-End Engineer",
+        description: "Key role involvement in developing Web based applications For Logistics enterpise.",
+        tags: ["Angular", "Java", "LoopBack", "MongoDB"],
+        duration: "May 2019 - March 2022",
+        demo: "http://react-job-listing-website.codebucks27.vercel.app/",
+        github: "https://github.com/codebucks27/react-job-listing-website"
+
+    },
+    {
+        id: 3,
+        name: "Virtusa PVT LTD",
+        role: "Junior Full Stack Developer",
+        description: "Played a key role in monitoring development activities and providing technical/process level guidance/support to the project team..",
+        tags: ["Angular", "Java", "WebRTC", "Socket.io"],
+        duration: "March 2018 - May 2019",
+        demo: "http://devto-clone-codebucks27.vercel.app/",
+        github: "https://github.com/codebucks27/devto-clone"
+    },
+   
+
+] 
