37eae7fb7a976aba45459da5bef265ccae9314ce
ShyamVirat <k_komaravolu@fanshaweonline.ca>
2024-01-05 13:38:02 -0500

    reveiwed

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -4,7 +4,7 @@ export const Work = [
     {
         id: 1,
         name: "Colossus Consulting Inc",
-        role: "Senior Full Stack Developer",
+        role: "Front end engineer with JS",
         description: "Bulit and managed multiple Full stack applications for a Bank and handled a different teams for multiple projects.",
         tags: ["React", "Angular", "NodeJS", "MongoDB"],
         duration: "March 2022 - Present",
@@ -14,24 +14,24 @@ export const Work = [
     {
         id: 2,
         name: "Tata Consultancy Services",
-        role: "Senior Front-End Engineer",
-        description: "Key role involvement in developing Web based applications For Logistics enterpise.",
-        tags: ["Angular", "Java", "LoopBack", "MongoDB"],
+        role: "QA",
+        description: "Key role involvement in developing Web based application and testing ",
+        tags: ["Angular", "JAVASCRIPT", "LoopBack", "SQLSERVER"],
         duration: "May 2019 - March 2022",
         demo: "http://react-job-listing-website.codebucks27.vercel.app/",
         github: "https://github.com/codebucks27/react-job-listing-website"
 
     },
-    {
-        id: 3,
-        name: "Virtusa PVT LTD",
-        role: "Junior Full Stack Developer",
-        description: "Played a key role in monitoring development activities and providing technical/process level guidance/support to the project team..",
-        tags: ["Angular", "Java", "WebRTC", "Socket.io"],
-        duration: "March 2018 - May 2019",
-        demo: "http://devto-clone-codebucks27.vercel.app/",
-        github: "https://github.com/codebucks27/devto-clone"
-    },
+    // {
+    //     id: 3,
+    //     name: "Virtusa PVT LTD",
+    //     role: "Junior Full Stack Developer",
+    //     description: "Played a key role in monitoring development activities and providing technical/process level guidance/support to the project team..",
+    //     tags: ["Angular", "Java", "WebRTC", "Socket.io"],
+    //     duration: "March 2018 - May 2019",
+    //     demo: "http://devto-clone-codebucks27.vercel.app/",
+    //     github: "https://github.com/codebucks27/devto-clone"
+    // },
    
 
 ] 
